bd2c8f6f43d0fd27c7a6695aeea53d26a296fee6
Kudo Chien <kudo@cloudmosa.com>
2019-02-11 16:35:30 +0800

    Fix gtk+3 merge conflict and upgrade gtk to 3.24.5
    Summary:
        1. Remove original #1600 issue patch that already included in 3.24.5
        2. Update patch comment that add merge request ID for future tracking
        3. The pango fix is merged but not yet release in new version. Keep original patch.

--- Formula/gtk+3.rb
+++ Formula/gtk+3.rb
@@ -1,17 +1,9 @@
 class Gtkx3 < Formula
   desc "Toolkit for creating graphical user interfaces"
   homepage "https://gtk.org/"
-<<<<<<< HEAD
-  url "https://download.gnome.org/sources/gtk+/3.24/gtk+-3.24.3.tar.xz"
-  sha256 "5708fa534d964b1fb9a69d15758729d51b9a438471d4612dc153f595904803bd"
-  revision 7
-||||||| merged common ancestors
-  url "https://download.gnome.org/sources/gtk+/3.24/gtk+-3.24.3.tar.xz"
-  sha256 "5708fa534d964b1fb9a69d15758729d51b9a438471d4612dc153f595904803bd"
-=======
   url "https://download.gnome.org/sources/gtk+/3.24/gtk+-3.24.5.tar.xz"
   sha256 "0be5fb0d302bc3de26ab58c32990d895831e2b7c7418d0ffea1206d6a3ddb02f"
->>>>>>> upstream/master
+  revision 1
 
   bottle do
     sha256 "48b4596d4755c9ac359e26c2330775cfb3b3def44009ab88d895e5f861f80f46" => :mojave
@@ -30,21 +22,21 @@ class Gtkx3 < Formula
   depends_on "pango"
 
   # see https://gitlab.gnome.org/GNOME/gtk/issues/1593
+  # https://gitlab.gnome.org/GNOME/gtk/merge_requests/531
   patch do
     url "https://gitlab.gnome.org/GNOME/gtk/commit/ecfb540dabc58a5daef9c2f49230ae0f6f5c940e.diff"
     sha256 "1ac20e99a161941b9ce779194468f8ccfacdf21a05662f7b500e9edd8ccebd4e"
   end
 
-  # see https://gitlab.gnome.org/GNOME/gtk/issues/1600
-  patch :DATA
-
   # see https://gitlab.gnome.org/GNOME/gtk/issues/1618
+  # https://gitlab.gnome.org/GNOME/gtk/merge_requests/532
   patch do
     url "https://gitlab.gnome.org/GNOME/gtk/commit/cfad43b80d15328f2e82ccc677ce634bd32a5560.diff"
     sha256 "75f981cef20dbba9a56d843519a2f8fdc12ad88f4b4ced06150ceed6540886c6"
   end
 
   # Fix crash after close a fullscreen window.
+  # https://gitlab.gnome.org/GNOME/gtk/merge_requests/545
   patch do
     url "https://gitlab.gnome.org/GNOME/gtk/commit/7c88b88c67070cc15ac6cbc7e7479c6b23691b4a.diff"
     sha256 "aa02b7951ceff194c3a63043ffc96a3446ed103df6d0d7b0d3af94d099fb1261"
@@ -141,18 +133,3 @@ class Gtkx3 < Formula
     system "./test"
   end
 end
-
-__END__
-
-diff -u gtk+-3.24.3/modules/input/imquartz.c gtk+-3.24.3-working/modules/input/imquartz.c
---- gtk+-3.24.3/modules/input/imquartz.c        2019-01-13 19:06:51.000000000 -0800
-+++ gtk+-3.24.3-working/modules/input/imquartz.c        2019-01-16 17:54:36.000000000 -0800
-@@ -196,7 +196,7 @@
-     {
-       if (event->hardware_keycode == 0 && event->keyval == 0xffffff)
-         /* update text input changes by mouse events */
--        return output_result (context, win);
-+        return output_result (context, event->window);
-       else
-         return gtk_im_context_filter_keypress (qc->slave, event);
-     }
